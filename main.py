c69372eeb68c5c50217edc1d2791a501a89180f0
Ehsan Eslamian <eeslamian@gmail.com>
2024-05-15 11:58:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,18 +65,7 @@ def upload():
         filename_without_extension = os.path.splitext(filename_with_identifier)[0]
         logging.info(f"Filename without extension: {filename_without_extension}")
 
-        # Call your python3 scripts with the uploaded file as an argument
-        logging.info("calling your python3 scripts.")
-        subprocess.run(['python', 'Sipoc_to_pptx.py', csv_file_path])
-        logging.info("Sipoc_to_pptx.py executed successfully.")
-        subprocess.run(['python', 'Non-Core-Process-Statement.py', csv_file_path])
-        logging.info("Non_Core_Process_Statement.py executed successfully.")
-        subprocess.run(['python', 'Seperate_verbs.py', csv_file_path])
-        logging.info("Seperate_verbs.py executed successfully.")
-        subprocess.run(['python', 'Bracket.py', csv_file_path])
-        logging.info("Bracket.py executed successfully.")
-        subprocess.run(['python', 'Decision-Bubbles.py', csv_file_path])
-        logging.info("Decision_Bubbles.py executed successfully.")
+        # Call the script with the uploaded file as an argument
         subprocess.run(['python', 'RunAll.py', csv_file_path])
         logging.info("RunAll.py executed successfully.")
 
@@ -105,116 +94,6 @@ def download_all_files():
         # Handle exceptions
         logging.error(f'An error occurred while downloading all files: {str(e)}')
         return "An error occurred while downloading all files."
-   
-
-@app.route('/download_pptx')
-def download_pptx():
-    try:
-        # Get the file path from session
-        file_path = session.get('file_path')
-        if file_path:
-            # Specify the path to the PowerPoint file
-            filename_with_identifier = os.path.basename(file_path)
-            filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            pptx_filename = os.path.join('uploads', f'{filename_without_extension}.pptx')
-            logging.info("PowerPoint file downloaded successfully.")
-
-            # Return the file as an attachment
-            return send_file(pptx_filename, as_attachment=True)
-        else:
-            return "No file to download"
-    except Exception as e:
-        # Log any exceptions that occur
-        logging.error(f'An error occurred: {str(e)}')
-        # Return an error message or redirect to an error page
-        return "An error occurred while downloading the file."
-    
-@app.route('/download_NC_Statement')
-def download_NC_Statement():
-    try:
-        # Get the file path from session
-        file_path = session.get('file_path')
-        if file_path:
-            # Specify the path to the PowerPoint file
-            filename_with_identifier = os.path.basename(file_path)
-            filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            Non_cp_filename = os.path.join('uploads', filename_without_extension +'_non-cp-statement'+'.pptx')
-            logging.info("Non-CP-Statement file downloaded successfully.")
-
-            # Return the file as an attachment
-            return send_file(Non_cp_filename, as_attachment=True)
-        else:
-            return "No file to download"
-    except Exception as e:
-        # Log any exceptions that occur
-        logging.error(f'An error occurred: {str(e)}')
-        # Return an error message or redirect to an error page
-        return "An error occurred while downloading the file."
-
-
-@app.route('/download_verbs')
-def download_verbs():
-    try:
-        # Specify the path to the verbs.csv file
-        file_path = session.get('file_path')
-        if file_path:
-            filename_with_identifier = os.path.basename(file_path)
-            filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            verbs_pptxfile= os.path.join('uploads',filename_without_extension +'_verbs'+'.pptx')
-            logging.info("Verbs pptx file downloaded successfully.")
-
-
-        # Return the file as an attachment
-        return send_file(verbs_pptxfile, as_attachment=True)
-    except Exception as e:
-        # Log any exceptions that occur
-        logging.error(f'An error occurred: {str(e)}')
-        # Return an error message or redirect to an error page
-        return "An error occurred while downloading the file."
-    
-@app.route('/download_decision_bubbles')
-def download_decision_bubbles():
-        try:
-            # Get the file path from session
-            file_path = session.get('file_path')
-            if file_path:
-                # Specify the path to the PowerPoint file
-                filename_with_identifier = os.path.basename(file_path)
-                filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-                pptx_filename = os.path.join('uploads', filename_without_extension + '_decision-bubbles' + '.pptx')
-                logging.info("decision_bubbles pptx file downloaded successfully.")
-
-                # Return the file as an attachment
-                return send_file(pptx_filename, as_attachment=True)
-            else:
-                return "No file to download"
-        except Exception as e:
-            # Log any exceptions that occur
-            logging.error(f'An error occurred: {str(e)}')
-            # Return an error message or redirect to an error page
-            return "An error occurred while downloading the file."
-    
-    
-@app.route('/download_Sub_bubbles')
-def download_Sub_bubbles():
-    try:
-        file_path = session.get('file_path')
-        if file_path:
-            filename_with_identifier = os.path.basename(file_path)
-            filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            sub_bubble_pptxfile= os.path.join('uploads',filename_without_extension +'_subbubbles'+'.pptx')
-            logging.info(f"Sub_bubbles pptx file path {sub_bubble_pptxfile}")
-            logging.info("Sub_bubbles pptx file downloaded successfully.")
-
-        # Return the file as an attachment
-        return send_file(sub_bubble_pptxfile, as_attachment=True)
-    except Exception as e:
-        # Log any exceptions that occur
-        logging.error(f'An error occurred: {str(e)}')
-        # Return an error message or redirect to an error page
-        return "An error occurred while downloading the file."
-    
-
     
 
 if __name__ == '__main__':
